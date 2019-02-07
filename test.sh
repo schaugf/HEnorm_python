269253fb55cfea78e243ba08acd5829f10226a73
schaugf <schau.geoffrey@gmail.com>
2019-02-07 11:00:33 -0800

    cleanup

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python normalizeStaining.py --imageFile example1.png --saveFile norm1
-python normalizeStaining.py --imageFile example2.png --saveFile norm2
+python normalizeStaining.py --imageFile imgs/example1.png --saveFile imgs/norm1
+python normalizeStaining.py --imageFile imgs/example2.png --saveFile imgs/norm2
